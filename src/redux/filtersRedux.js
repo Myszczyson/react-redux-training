c580f99acc3f1e632f268b1518db044fb00f4284
Dawid Myszczynski <myszczym@gmail.com>
2021-06-04 11:40:15 +0200

    Remove tripRedux filteredId

--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -10,10 +10,17 @@ const createActionName = name => `app/${reducerName}/${name}`;
 
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
+export const ADD_TAG = createActionName('ADD_TAG');
+export const REMOVE_TAG = createActionName('REMOVE_TAG');
+export const CHANGE_DATE = createActionName('CHANGE_DATE');
 // TODO - add other action types
 
 // action creators
 export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
+export const addTag = payload => ({ payload, type: ADD_TAG});
+export const removeTag = payload => ({ payload, type: REMOVE_TAG});
+export const changeDateDuration = payload => ({ payload, type: CHANGE_DATE});
+
 // TODO - add other action creators
 
 // reducer
@@ -23,6 +30,21 @@ export default function reducer(statePart = [], action = {}) {
       return {
         ...statePart,
         searchPhrase: action.payload,
+      };
+    case ADD_TAG:
+      return {
+        ...statePart,
+        tags: action.payload,
+      };
+    case REMOVE_TAG:
+      return {
+        ...statePart,
+        tags: action.payload,
+      };
+    case CHANGE_DATE:
+      return {
+        ...statePart,
+        duration: action.payload,
       };
     // TODO - handle other action types
     default:
--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -21,24 +21,18 @@ export const getFilteredTrips = ({trips, filters}) => {
 };
 
 export const getTripById = ({trips}, tripId) => {
-  const filteredId = tripId;
-  const filtered = trips.filter(trip => trip.id == filteredId);
-
+  const filtered = trips.filter(trip => trip.id == tripId);
 
   // TODO - filter trips by tripId
 
-
-  console.log(filtered);
   return filtered.length ? filtered[0] : {error: true};
 };
 
 export const getTripsForCountry = ({trips}, countryCode) => {
-  const filteredCode = countryCode;
-  const filtered = trips.filter(trip => trip.country.code == filteredCode);
+  const filtered = trips.filter(trip => trip.country.code == countryCode);
 
   // TODO - filter trips by countryCode
 
-  console.log(filtered);
   return filtered.length ? filtered : [{error: true}];
 };
 
