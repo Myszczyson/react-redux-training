511640a35ab4c4a1e8d358a097045b573f69311a
Dawid Myszczynski <myszczym@gmail.com>
2021-06-07 14:22:20 +0200

    Add OrderSummary prica calculate function

--- src/components/features/OrderSummary/OrderSummary.js
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './OrderSummary.scss';
 import PropTypes from 'prop-types';
-// import formatPrice from '../../../utils/formatPrice';
-// import calculateTotal from '../../../utils/calculateTotal';
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
 
 const OrderSummary = props => (
 
-  <h2 className={styles.component}>{'Total:'}<strong>{props.tripCost}</strong></h2>
+  <h2 className={styles.component}>{'Total:'}<strong>{formatPrice(calculateTotal(props.tripCost, props.options))}</strong></h2>
 
 );
 
